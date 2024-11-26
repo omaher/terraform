44c33c8aa98a4850382bbc289845c1aeaf7ff557
Omkar Aher <aher.omkar.15et7021@gmail.com>
2024-11-27 00:30:48 +0530

    Updated module path for default workspace

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -1,5 +1,5 @@
 module "network" {
-  source                     = "C:/Users/HP/OneDrive/Desktop/terraform/modules/network"
+  source                     = "${env.WORKSPACE}/modules/network"
   AWS_AZ_1                   = var.AWS_AZ_1
   AWS_AZ_2                   = var.AWS_AZ_2
   vpc-cidr_block             = var.vpc-cidr_block
@@ -10,7 +10,7 @@ module "network" {
 }
 
 module "compute" {
-  source            = "C:/Users/HP/OneDrive/Desktop/terraform/modules/compute"
+  source            = "${env.WORKSPACE}/modules/compute"
   AWS_REGION        = var.AWS_REGION
   AMIS              = var.AMIS
   ecommerce_vpc-id  = module.network.ecommerce_vpc-id
